38a1c4af02d8d1195e7046464c8d934fe9400c2f
Vajramg <vajramg@gmail.com>
2024-08-22 20:48:24 +0530

    tf files update

--- Jenkins-server-TF/ec2.tf
+++ Jenkins-server-TF/ec2.tf
@@ -1,6 +1,9 @@
-resource "aws_instance" "Jenkins-server" {
-  ami                     = "ami-0dcc1e21636832c5d"
+resource "aws_instance" "var.ec2_name" {
+  ami                     = var.aws_instance_ami
   instance_type           = "t2.2xlarge"
+  key_name                = var.key_name
+  subnet_id               = aws_subnet.public-subnet.id
+  vpc_security_group_ids  = aws_security_group.jenkins-sg.id
   host_resource_group_arn = "arn:aws:resource-groups:us-west-2:012345678901:group/win-testhost"
   tenancy                 = "host"
 }
